535265f0ab10cbd7a2dcfea116482e0d6aa661ae
muhamammed <muhammed@technowavegroup.com>
2022-11-08 11:53:34 +0530

    Main thread to Bg thread

--- app/src/main/java/com/technowave/broadcastexample/MyReceiver.kt
+++ app/src/main/java/com/technowave/broadcastexample/MyReceiver.kt
@@ -5,9 +5,26 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import com.technowave.broadcastexample.MainActivity.Companion.TAG
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class MyReceiver: BroadcastReceiver() {
+    private val scope = CoroutineScope(SupervisorJob())
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.d(TAG, "Received event")
+
+        val pendingResult: PendingResult = goAsync()
+        scope.launch(Dispatchers.Default) {
+            try {
+
+            } catch (e : Exception) {
+
+            } finally {
+                pendingResult.finish()
+            }
+        }
     }
 }
